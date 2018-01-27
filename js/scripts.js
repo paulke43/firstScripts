0cba99aae2dfba1b3bb4f0d4dbfa1813085ef685
Pawel Morka <morka.paul@gmail.com>
2018-01-27 12:34:16 +0100

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,3 @@
-// To jest komentarz liniowy
-
 var name = prompt('Enter your name');
-// alert('Hello, ' + name);
+alert('Hello, ' + name);
 console.log('Hello, ' + name); 
-
-/*
-var variable = 'test';
-console.log(variable); 
-*/
